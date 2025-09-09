0e43417e5cf3aa49f2593d50e73a8e8cf39c88b8
SpaceXe-tech <billaxe299@gmail.com>
2025-09-09 11:45:01 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -1,37 +1,40 @@
-from pyrogram.types import InlineKeyboardButton
-
-import config
 from AnonXMusic import app
-
+from pyrogram.types import InlineKeyboardButton
 
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text="ᴡᴇʙ ᴘʟᴀʏᴇʀ",
+                url=f"https://t.me/{app.username.lstrip('@')}?startapp"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+        [
+            InlineKeyboardButton(
+                text="ʟᴀɴɢᴜᴀɢᴇ",
+                callback_data="LG"
+            ),
         ],
     ]
     return buttons
-
 
 def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+                text=_["S_B_4"],
+                callback_data="settings_back_helper"
+            ),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(
+                text="ʟᴀɴɢᴜᴀɢᴇ",
+                callback_data="LG"
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_6"],
+                url=config.SUPPORT_CHANNEL
+            ),
         ],
     ]
     return buttons
