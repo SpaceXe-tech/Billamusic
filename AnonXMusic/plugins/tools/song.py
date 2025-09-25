fdd16983d2b6452ffafdcac663abb0b69fd4bc94
STRX70 <salonisitole2305@gmail.com>
2025-09-25 23:02:24 +0530

    Update song.py

--- AnonXMusic/plugins/tools/song.py
+++ AnonXMusic/plugins/tools/song.py
@@ -62,7 +62,7 @@ async def search_song(client, message):
         song_storage[callback_data] = song
 
     await message.reply_text(
-        "**🎵 ꜱᴇʟᴇᴄᴛ ᴀ ꜱᴏɴɢ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ:**",
+        "│ꜱᴇʟᴇᴄᴛ ᴀ ꜱᴏɴɢ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ 🎵:\n└ʙʏ @Stormifybot",
         reply_markup=InlineKeyboardMarkup(buttons)
     )
 
